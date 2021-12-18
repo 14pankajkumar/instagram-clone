0a6714b37adb5f736ebb19f74bd692d3020d8825
Pankaj Kumar <pankajkumardas727@gmail.com>
2021-12-18 23:14:30 +0530

    bug fixed

--- pages/[userprofile].js
+++ pages/[userprofile].js
@@ -1,11 +1,15 @@
 import { getSession, useSession } from "next-auth/react";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import Header from "../components/Header";
 import Modal from "../components/Modal";
 import UserFeed from "../components/UserFeed";
 
-const UserProfile = ({ usernameURL }) => {
+const UserProfile = () => {
   const { data: session } = useSession();
+  const router = useRouter();
+  const { userprofile } = router.query;
+
   return (
     <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
       <Head>
@@ -16,13 +20,13 @@ const UserProfile = ({ usernameURL }) => {
       <Header />
 
       {/* User Feed */}
-      {/* {session && usernameURL === session.user.username ? (
+      {session && session.user.username === userprofile ? (
         <UserFeed />
       ) : (
         <div className="flex justify-center mt-56">
           <h1 className="font-bold text-2xl">Please Sign In</h1>
         </div>
-      )} */}
+      )}
 
       {/* Modal */}
       <Modal />
@@ -33,11 +37,9 @@ const UserProfile = ({ usernameURL }) => {
 export default UserProfile;
 
 export async function getServerSideProps(context) {
-  const usernameURL = context.query.userprofile;
   return {
     props: {
       session: await getSession(context),
-      usernameURL,
     },
   };
 }
